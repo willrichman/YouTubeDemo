5edc7d97557b2f76766755649ccc3d5a5903867e
Will Richman <wrichman@gmail.com>
2015-04-27 15:02:46 -0700

    Refactored code for new video loads.

--- YouTubeDemo/PlayerViewController.swift
+++ YouTubeDemo/PlayerViewController.swift
@@ -51,14 +51,7 @@ class PlayerViewController: UIViewController, YTPlayerViewDelegate {
     // MARK: IBAction functions
     
     @IBAction func pressedNewVideo(sender: AnyObject) {
-        self.playerView.stopVideo()
-        NetworkController.controller.returnRandomVideoID { (id, errorDescription) -> Void in
-            if errorDescription != nil {
-                println(errorDescription)
-            } else {
-                self.playerView.loadVideoById(id!, startSeconds: 0, suggestedQuality: YTPlaybackQuality.Large)
-            }
-        }
+        self.startNewVideo()
     }
     
     // MARK: YTPlayerViewDelegate functions
@@ -67,23 +60,22 @@ class PlayerViewController: UIViewController, YTPlayerViewDelegate {
         println(state.value)
         if state.value == 1 {
             // If player ends, add new video
-            self.playerView.stopVideo()
-            NetworkController.controller.returnRandomVideoID { (id, errorDescription) -> Void in
-                if errorDescription != nil {
-                    println(errorDescription)
-                } else {
-                    self.playerView.loadVideoById(id!, startSeconds: 0, suggestedQuality: YTPlaybackQuality.Large)
-                }
-            }
+            self.startNewVideo()
         } else if state.value == 0 {
             // If player can't load video, start new video
-            self.playerView.stopVideo()
-            NetworkController.controller.returnRandomVideoID { (id, errorDescription) -> Void in
-                if errorDescription != nil {
-                    println(errorDescription)
-                } else {
-                    self.playerView.loadVideoById(id!, startSeconds: 0, suggestedQuality: YTPlaybackQuality.Large)
-                }
+            self.startNewVideo()
+        }
+    }
+    
+    // MARK: Custom video load function
+    
+    func startNewVideo() {
+        self.playerView.stopVideo()
+        NetworkController.controller.returnRandomVideoID { (id, errorDescription) -> Void in
+            if errorDescription != nil {
+                println(errorDescription)
+            } else {
+                self.playerView.loadVideoById(id!, startSeconds: 0, suggestedQuality: YTPlaybackQuality.Large)
             }
         }
     }
