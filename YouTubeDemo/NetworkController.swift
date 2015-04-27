921bcac91146526abcf6e406522c2088bfc529d7
Will Richman <wrichman@gmail.com>
2015-04-27 12:42:37 -0700

    Added functions to NetworkController to populate a random month for video search.

--- YouTubeDemo/NetworkController.swift
+++ YouTubeDemo/NetworkController.swift
@@ -13,7 +13,7 @@ class NetworkController {
     let APIKey = "AIzaSyCjepqbIjcTQnnsKzoiAPZDeueEaUyBCow"
     let baseTime: Double = 1114214400 - NSTimeIntervalSince1970 // Day of first YouTube video
     let timeInterval: Double = 2629740  // Approximately one month
-    var maxTime: Double?
+    var maxTime: Double
     var notYetPlayed = [String]()
     var alreadyPlayed = Dictionary<String, Bool>()
     let RFC3339DateFormatter = NSDateFormatter()
@@ -30,4 +30,44 @@ class NetworkController {
         }
         return Static.instance!
     }
+    
+    init() {
+        self.RFC3339DateFormatter.locale = enUSPOSIXLocale
+        self.RFC3339DateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
+        self.RFC3339DateFormatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        let currentDate = NSDate().timeIntervalSince1970
+        self.maxTime = currentDate - self.timeInterval
+    }
+    
+    // MARK: Video retrieval functions
+    
+    func returnRandomVideoID() -> String? {
+        if self.notYetPlayed.isEmpty {
+            populateRandomVideoSet()
+        }
+        let randomIndex = Int(arc4random_uniform(UInt32(self.notYetPlayed.count - 1)))
+        var videoToPlay = self.notYetPlayed[randomIndex]
+        self.notYetPlayed.removeAtIndex(randomIndex)
+        self.alreadyPlayed[videoToPlay] = true
+        return videoToPlay
+    }
+    
+    func populateRandomVideoSet() {
+        let baseQuery = "key=" + APIKey + "&q=cats&part=id&type=video&videoDuration=short&videoEmbeddable=true"
+        let monthTuple = returnRandomMonth()
+        let startDate = monthTuple.0
+        let endDate = monthTuple.1
+        let fullQuery = baseQuery + "&publishedAfter=" + startDate! + "&publishedBefore=" + endDate!
+    }
+    
+    func returnRandomMonth() -> (String?, String?) {
+        let startTime = Double(arc4random_uniform(UInt32(self.maxTime - self.baseTime))) + self.baseTime
+        let endTime = startTime + timeInterval
+        let startTimeRFC = self.RFC3339DateFormatter.stringFromDate(NSDate(timeIntervalSinceReferenceDate: startTime))
+        println(startTimeRFC)
+        let endTimeRFC = self.RFC3339DateFormatter.stringFromDate(NSDate(timeIntervalSinceReferenceDate: endTime))
+        println(endTimeRFC)
+        return (startTimeRFC, endTimeRFC)
+    }
 }
+
